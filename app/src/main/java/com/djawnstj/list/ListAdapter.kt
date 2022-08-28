1bd745c5fd16267095e76d4bc55dc6c2f2651592
junseo <djawnstj@naver.com>
2022-08-28 10:32:37 +0900

    finish ListAdapter

--- app/src/main/java/com/djawnstj/list/ListAdapter.kt
+++ app/src/main/java/com/djawnstj/list/ListAdapter.kt
@@ -1,6 +1,42 @@
 package com.djawnstj.list
 
-class ListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.djawnstj.list.databinding.ItemListBinding
+
+class ListAdapter: ListAdapter<User, com.djawnstj.list.ListAdapter.ViewHolder>(diffUtil), ItemTouchHelperListener {
+
+    override fun onItemMove(from: Int, to: Int) {
+        val item: User = currentList[from]
+        val newList = ArrayList<User>()
+        newList.addAll(currentList)
+        newList.removeAt(from)
+        newList.add(to, item)
+        submitList(newList)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(ItemListBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+        holder.setItem(getItem(position), position)
+
+    inner class ViewHolder(private val binding: ItemListBinding): RecyclerView.ViewHolder(binding.root) {
+        fun setItem(item: User, position: Int) {
+            binding.holderText.text = "${item.id}. ${item.name}(${item.age})"
+        }
+    }
+
+    companion object {
+        private const val TAG = "ListAdapter"
+        private val diffUtil = object: DiffUtil.ItemCallback<User>() {
+            override fun areItemsTheSame(oldItem: User, newItem: User): Boolean = oldItem.id == newItem.id
+            override fun areContentsTheSame(oldItem: User, newItem: User): Boolean = oldItem == newItem
+        }
+    }
 
 
 }
--- app/src/main/java/com/djawnstj/list/MainActivity.kt
+++ app/src/main/java/com/djawnstj/list/MainActivity.kt
@@ -17,11 +17,13 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     // 리싸이클러뷰 어뎁터에 들어갈 아이템들이 담긴 배열 변수
-    private val list by lazy { mutableListOf<String>() }
+    private val list by lazy { mutableListOf<User>() }
     // 리싸이클러뷰 어뎁터
     private val recyclerViewAdapter by lazy { RecyclerViewAdapter() }
+    // 리스트 어댑터
+    private val listAdapter by lazy { ListAdapter() }
     // 리싸이클러뷰 아이템 이동 콜백 변수
-    private val itemTouchHelper by lazy { ItemTouchHelper(ItemTouchCallback(recyclerViewAdapter)) }
+    private val itemTouchHelper by lazy { ItemTouchHelper(ItemTouchCallback(listAdapter)) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +36,7 @@ class MainActivity : AppCompatActivity() {
     /** 리싸이클러뷰 초기화 함수 */
     private fun initRecyclerView() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        binding.recyclerView.adapter = recyclerViewAdapter
+        binding.recyclerView.adapter = listAdapter
 
         // 리싸이클러뷰에 itemTouchHelper 연결
         itemTouchHelper.attachToRecyclerView(binding.recyclerView)
@@ -45,9 +47,13 @@ class MainActivity : AppCompatActivity() {
     /** 리싸이클러뷰 어뎁터에 아이템 넣는 함수 */
     private fun setListItems() {
         for (i in 0..20) {
-            list.add("$i 번째 텍스트")
+            list.add(User(
+                i.toLong(),
+                "홍길동",
+                i+20
+            ))
         }
-        recyclerViewAdapter.submitList(list)
+        listAdapter.submitList(list)
     }
 
 }
